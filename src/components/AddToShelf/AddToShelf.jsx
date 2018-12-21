bbe4a8f5cfcb369870466d3af706894a43f0de81
JD Ghuman <jdsghuman@gmail.com>
2018-12-21 11:23:22 -0600

    Add styling to components

--- src/components/AddToShelf/AddToShelf.jsx
+++ src/components/AddToShelf/AddToShelf.jsx
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
-import {connect} from 'react-redux';
-
-
+import { connect } from 'react-redux';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 class AddToShelf extends Component {
 
     state = {
-        description:'',
+        description: '',
         image: ''
-    }  
+    }
 
-    handleChange = (event) =>{
+    handleChange = (event) => {
         console.log(`${event.target.name} changed to ${event.target.value}`);
         this.setState({
             [event.target.name]: event.target.value
@@ -19,27 +19,65 @@ class AddToShelf extends Component {
 
     handleClick = () => {
         this.props.dispatch({
-            type:   'POST_ITEM',
+            type: 'POST_ITEM',
             payload: this.state
         })
 
         this.setState({
-            description:'',
+            description: '',
             image: ''
         })
     }
 
-    render(){
-        return(
-            <React.Fragment>
-                <input name={'description'} onChange={this.handleChange} type="text" placeholder="Description"/>
-                <input name={'image'} onChange={this.handleChange} type="text" placeholder="Image"/>
-                <button onClick={this.handleClick}>
-                    Submit
-                </button>
-            </React.Fragment>
+    render() {
+        return (
+            <div style={divContainer}>
+                <TextField name={'description'}
+                    style={inputStyle}
+                    label="Description"
+                    onChange={this.handleChange}
+                    margin="normal"
+                    variant="outlined"
+                    placeholder="Description"
+                    value={this.state.description}
+                />
+                <TextField
+                    style={inputStyle}
+                    name={'image'}
+                    label="Image"
+                    onChange={this.handleChange}
+                    margin="normal"
+                    variant="outlined"
+                    placeholder="Image"
+                    value={this.state.image}
+                />
+                <Button
+                    style={btnStyle}
+                    onClick={this.handleClick}
+                    variant="contained"
+                    color="primary"
+                    size="large" >
+                    Add Image
+                </Button>
+            </div>
         )
     }
 };
+
+const btnStyle = {
+    display: 'block',
+    margin: '0 auto',
+    marginTop: '20px'
+}
+
+const inputStyle = {
+    margin: '10px'
+}
+
+const divContainer = {
+    margin: '0 auto',
+    marginTop: '40px',
+    textAlign: 'center'
+}
 
 export default connect()(AddToShelf);
--- src/components/Shelf/Shelf.js
+++ src/components/Shelf/Shelf.js
@@ -1,34 +1,39 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Button from '@material-ui/core/Button';
+import DeleteIcon from '@material-ui/icons/Delete';
+import Grid from '@material-ui/core/Grid';
 
 // This is one of our simplest components
 // It doesn't have local state, so it can be a function component.
 // It doesn't dispatch any redux actions or display any part of redux state
 // or even care what the redux state is, so it doesn't need 'connect()'
 
-const imgStyles = {
-  maxWidth: '200px'
-}
-
 class Shelf extends Component {
 
   componentDidMount = () => {
-    this.props.dispatch({type: 'FETCH_ITEMS'})
-    
+    this.props.dispatch({ type: 'FETCH_ITEMS' })
+
   }
 
   deleteItem = (item) => {
-    this.props.dispatch({type: 'DELETE_ITEMS', payload: item.id})
+    this.props.dispatch({ type: 'DELETE_ITEMS', payload: item.id })
   }
 
   render() {
     let display = this.props.reduxStore.shelfReducer.map(item => {
-      return <li key={item.id}><img style={imgStyles} src={item.image_url}/><p>{item.description}</p><button onClick={() => this.deleteItem(item)}>Delete</button></li>
+      return <li style={listStyles} key={item.id}><img style={imgStyles} src={item.image_url} /><p>{item.description}</p>
+        <Button onClick={() => this.deleteItem(item)} variant="contained" color="secondary">
+          Delete
+          <DeleteIcon />
+        </Button></li>
     })
-    return(
-      <ul>
-        {display}
-      </ul>
+    return (
+      <Grid container item xs={12}>
+        <ul>
+          {display}
+        </ul>
+      </Grid>
     )
   }
 }
@@ -36,6 +41,18 @@ const mapStateToProps = (reduxStore) => {
   return {
     reduxStore
   }
+}
+
+const listStyles = {
+  listStyle: 'none',
+  margin: '20px',
+  display: 'inline-block'
+}
+
+const imgStyles = {
+  maxWidth: '200px',
+  height: '180px',
+  overflow: 'hidden'
 }
 
 export default connect(mapStateToProps)(Shelf);
